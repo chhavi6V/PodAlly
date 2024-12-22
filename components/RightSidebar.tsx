69c5c8d597e0ab8221a5d63be9481b337823ce89
chhavi6V <chhavigupta@Chhavis-MacBook-Air.local>
2024-12-22 18:05:10 +0530

    first commit

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <section className='text-white-1 right_sidebar'>
+        Right sidebar
+    </section>
+  )
+}
+
+export default RightSidebar
---
+++ constants/index.ts
@@ -0,0 +1,103 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/discover.svg",
+    route: "/discover",
+    label: "Discover",
+  },
+  {
+    imgURL: "/icons/microphone.svg",
+    route: "/create-podcast",
+    label: "Create Podcast",
+  },
+];
+
+export const voiceDetails = [
+  {
+    id: 1,
+    name: "alloy",
+  },
+  {
+    id: 2,
+    name: "echo",
+  },
+  {
+    id: 3,
+    name: "fable",
+  },
+  {
+    id: 4,
+    name: "onyx",
+  },
+  {
+    id: 5,
+    name: "nova",
+  },
+  {
+    id: 6,
+    name: "shimmer",
+  },
+];
+
+export const podcastData = [
+  {
+    id: 1,
+    title: "The TTT Podcast",
+    description: "A weekly Comedy, News and Sports podcast",
+    imgURL:
+      "https://cachedimages.podchaser.com/512x512/aHR0cHM6Ly9hc3NldHMucGlwcGEuaW8vc2hvd3MvY292ZXIvMTcxNDQwNjQ0MjkwMS0zMDdmMzc5ODk2NzZjN2I2YmYwNDdlZmI3ZTJjN2FlNi5qcGVn/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D",
+  },
+  {
+    id: 2,
+    title: "Midnight Burger",
+    description: "A must-listen indie podcast",
+    imgURL:
+      "https://cachedimages.podchaser.com/512x512/aHR0cHM6Ly9tZWdhcGhvbmUuaW1naXgubmV0L3BvZGNhc3RzL2IwNTBkNmEyLTU5YzAtMTFlYy04NWVlLWYzZWFiNjBmY2YyNS9pbWFnZS9iZDk5ZGQ0ZDA3MDQ4YzA4NTFlMzRmMzdkNDAyZjcwMS5qcGc%2FaXhsaWI9cmFpbHMtNC4zLjEmbWF4LXc9MzAwMCZtYXgtaD0zMDAwJmZpdD1jcm9wJmF1dG89Zm9ybWF0JTJDY29tcHJlc3M%3D/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D",
+  },
+  {
+    id: 3,
+    title: "Health Hacker",
+    description: "The guide to hacking life - to feel, look and live better",
+    imgURL:
+      "https://cachedimages.podchaser.com/512x512/aHR0cHM6Ly93d3cub21ueWNvbnRlbnQuY29tL2QvcHJvZ3JhbXMvODIwZjA5Y2YtMmFjZS00MTgwLWE5MmQtYWE0YzAwMDhmNWZiLzYxNjhlZjg4LTM2MjEtNGYwOC1hN2JjLWFhNGMwMDNmYTA0Yi9pbWFnZS5qcGc%2FdD0xNjEzNjAwMzEwJnNpemU9TGFyZ2U%3D/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D",
+  },
+  {
+    id: 4,
+    title: "Tales Of The Beard",
+    description: "A weekly Fiction, Drama and Science Fiction podcast",
+    imgURL:
+      "https://cachedimages.podchaser.com/512x512/aHR0cHM6Ly9hc3NldHMucGlwcGEuaW8vc2hvd3MvY292ZXIvMTcyNTQ2NDE2MzMxNi0yZmUyOGI2NS04NTNhLTQyMTEtYjMzNy05MzE3YjhkODk2NDAuanBlZw%3D%3D/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D",
+  },
+  {
+    id: 5,
+    title: "Today, Explained",
+    description: "A long form, in-depth conversation",
+    imgURL:
+      "https://cachedimages.podchaser.com/512x512/aHR0cHM6Ly9tZWdhcGhvbmUuaW1naXgubmV0L3BvZGNhc3RzLzIzZDUyYTJhLTFjNWYtMTFlYS05YTBlLWI3MDE3MGYyYTgyNy9pbWFnZS91cGxvYWRzXzJGMTU4ODM1NzExMzM2Ni1qanNkZnp4Nng0bS1jMDMzOWMxMGY5YjExM2E1ZmNjOTM0MzZlNjZlNWVmNF8yRlRvZGF5RXhwbGFpbmVkK1YyX1RpbGVfMzAwMHgzMDAwK18yODFfMjkucG5nP2l4bGliPXJhaWxzLTQuMy4xJm1heC13PTMwMDAmbWF4LWg9MzAwMCZmaXQ9Y3JvcCZhdXRvPWZvcm1hdCUyQ2NvbXByZXNz/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D",
+  },
+  {
+    id: 6,
+    title: "Talk Tuah with Haliey Welch",
+    description: "Join Michelle Obama in conversation",
+    imgURL:
+      "https://cachedimages.podchaser.com/512x512/aHR0cHM6Ly9tZWdhcGhvbmUuaW1naXgubmV0L3BvZGNhc3RzLzYwYjRlMjVjLTdkMDEtMTFlZi1iOTkzLTc3OGI2MDc0ZmZlMy9pbWFnZS83NTk4YWQxNDZjMmViZmQ5MzliMjg3YzVhYWI3NDQ4MC5qcGc%2FaXhsaWI9cmFpbHMtNC4zLjEmbWF4LXc9MzAwMCZtYXgtaD0zMDAwJmZpdD1jcm9wJmF1dG89Zm9ybWF0JTJDY29tcHJlc3M%3D/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D",
+  },
+  {
+    id: 7,
+    title: "Stay Outta My Fridge",
+    description: "A long form, in-depth conversation",
+    imgURL:
+      "https://cachedimages.podchaser.com/512x512/aHR0cHM6Ly9hc3NldHMucGlwcGEuaW8vc2hvd3MvNjQ2M2E4NDAwM2RkZDEwMDExNGI2MGQ5LzE3MTU2MzgyNTc1NzYtMWU2MDQwMjMwYzFiYTExNTM2MWJhYzMxMmQwZjg4ZGQuanBlZw%3D%3D/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D",
+  },
+  {
+    id: 8,
+    title: "Value Based Care Advisory (VBCA) Podcast",
+    description: "This is how the news should sound",
+    imgURL:
+      "https://cachedimages.podchaser.com/512x512/aHR0cHM6Ly9hcnR3b3JrLmNhcHRpdmF0ZS5mbS9iY2YyMjM4OC00MzdlLTQxMDUtYTZhMy1mOTI1ZWM5ZTFlMjQvMTM0NTE3MTMtMTYyMjg4MDYzMTQxNC1jM2ZjNDg1ZTdlYzkxLmpwZw%3D%3D/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D",
+  },
+];
